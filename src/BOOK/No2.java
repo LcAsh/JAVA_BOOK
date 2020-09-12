8d298f02ac1a88111910a4098bfd0765d3edc52d
LcAsh <781073897@qq.com>
2020-09-12 23:07:24 +0800

    first homework

---
+++ src/BOOK/No2.java
@@ -0,0 +1,10 @@
+package BOOK;
+
+public class No2 {
+    private int serialNumber;
+    private static int counter = 0;
+    public void Count (){
+        counter++;
+        serialNumber = counter;
+    }
+}
